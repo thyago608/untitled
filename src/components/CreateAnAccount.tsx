bcf64db1383e785b541dcfaae7f9c186d9b3e2c4
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-09-29 23:12:17 -0400

    Created and Styled Component Form

--- src/components/CreateAnAccount.tsx
+++ src/components/CreateAnAccount.tsx
@@ -5,7 +5,7 @@ export function CreateAnAccount() {
   return (
     <aside className="h-[100vh] px-2 py-6 bg-slate-50">
       <Coffee size={32} color="#333" />
-      <div className="flex flex-col mt-2">
+      <div className="flex flex-col mt-2 mb-6">
         <h2 className="text-2xl font-bold text-[#333]">Create an account</h2>
         <span className="text-sm text-zinc-500">
           Let's get started with your 30 days free trial.
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -7,11 +7,16 @@ export function Form() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
       <Input type="text" placeholder="name" />
       <Input type="email" placeholder="email" />
       <Input type="password" placeholder="password" />
-      <button type="submit">create account</button>
+      <button
+        type="submit"
+        className="mt-5 w-[100%] h-10 bg-stone-900 text-zinc-100 text-sm rounded-[4px] hover:bg-stone-700 transition-colors"
+      >
+        Create account
+      </button>
     </form>
   );
 }
